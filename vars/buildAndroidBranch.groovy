321a2144aad6d64c9ec9c17f6b86d0cd25d21658
HLXEasy <hlxeasy@gmail.com>
2020-12-03 23:00:45 +0100

    Added new helper functions for Android builds

---
+++ vars/buildAndroidBranch.groovy
@@ -0,0 +1,35 @@
+// ----------------------------------------------------------------------------
+// SPDX-FileCopyrightText: © 2020 Alias Developers
+//
+// SPDX-License-Identifier: MIT
+//
+// @author HLXEasy <hlxeasy@gmail.com>>
+//
+// ----------------------------------------------------------------------------
+
+def call(Map params = [:]) {
+    String dockerfile = params.get("dockerfile")
+    String dockerTag = params.get("dockerTag")
+    String gitTag = params.get("gitTag")
+    String gitCommit = params.get("gitCommit")
+    String githubToken = params.get("githubCIToken")
+    withDockerRegistry(credentialsId: 'DockerHub-Login') {
+        withCredentials([string(credentialsId: 'Android-Sign-Keystore', variable: 'KEYSTORE_PASS')]) {
+            sh(
+                    script: """
+                    docker build \
+                        -f ${dockerfile} \
+                        --rm \
+                        --build-arg GITHUB_CI_TOKEN=${githubToken} \
+                        --build-arg GIT_COMMIT=${gitCommit} \
+                        --build-arg ALIAS_RELEASE=${gitTag} \
+                        --build-arg REPLACE_EXISTING_ARCHIVE=--replace \
+                        --build-arg KEYSTORE_PASS=${KEYSTORE_PASS} \
+                        -t ${dockerTag} \
+                        -v /etc/ssl/certs/AliasWalletAndroidKeyStore.jks:/etc/ssl/certs/alias-sign-keystore.jks \
+                        .
+                """
+            )
+        }
+    }
+}
---
+++ vars/buildAndroidFeatureBranch.groovy
@@ -0,0 +1,28 @@
+// ----------------------------------------------------------------------------
+// SPDX-FileCopyrightText: © 2020 Alias Developers
+//
+// SPDX-License-Identifier: MIT
+//
+// @author HLXEasy <hlxeasy@gmail.com>>
+//
+// ----------------------------------------------------------------------------
+
+def call(Map params = [:]) {
+    String dockerfile = params.get("dockerfile")
+    String dockerTag = params.get("dockerTag")
+    withDockerRegistry(credentialsId: 'DockerHub-Login') {
+        withCredentials([string(credentialsId: 'Android-Sign-Keystore', variable: 'KEYSTORE_PASS')]) {
+            sh(
+                    script: """
+                    docker build \
+                        -f $dockerfile \
+                        --rm \
+                        --build-arg KEYSTORE_PASS=${KEYSTORE_PASS} \
+                        -t $dockerTag \
+                        -v /etc/ssl/certs/AliasWalletAndroidKeyStore.jks:/etc/ssl/certs/alias-sign-keystore.jks \
+                        .
+                """
+            )
+        }
+    }
+}
